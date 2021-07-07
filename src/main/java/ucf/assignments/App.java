138861974b8f3756fabb9cba68688371cf3b12ea
Akwill47 <wil.weat@hotmail.com>
2021-07-07 10:03:20 -0400

    s

--- src/main/java/ucf/assignments/App.java
+++ src/main/java/ucf/assignments/App.java
@@ -17,6 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        System.out.println("hello");
         try {
             Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
             Scene scene = new Scene(root);
